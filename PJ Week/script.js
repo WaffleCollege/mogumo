9021126176f67a99fd3dbedec2bb66273db698c4
kurumayusamugariko <junmomo6780@gmail.com>
2023-08-26 06:45:24 +0900

    4OK

--- PJ Week/script.js
+++ PJ Week/script.js
@@ -87,23 +87,9 @@ game("パー");
 
 //問題４
 // ここに関数を作成しましょう。
-function flatten(input) {
-  const stack = [...input];
-  const res = [];
-  while (stack.length) {
-    // pop value from stack
-    const next = stack.pop();
-    if (Array.isArray(next)) {
-      // push back array items, won't modify the original input
-      stack.push(...next);
-    } else {
-      res.push(next);
-    }
-  }
-  // reverse to restore input order
-  return res.reverse();
+function flatten(array) {
+	return [].concat(...array);
 }
-
 
 console.log(
 	flatten([
