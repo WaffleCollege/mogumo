eb86453dca84564a3ca4b34acaac8f76dc831e87
kurumayusamugariko <junmomo6780@gmail.com>
2023-08-26 06:23:05 +0900

    3OK

--- PJ Week/script.js
+++ PJ Week/script.js
@@ -39,3 +39,48 @@ function greatestcd(x,y){
 }
 test(greatestcd(33,11), 11);
 test(greatestcd(11,33), 11);
+
+//問題３
+const hands = new Array("パー", "グー", "チョキ");
+const player = "パー"
+
+/**
+    * @param {int} min - ランダムで出したい値の最小値
+    * @param {int} max - ランダムで出したい値の最大値より1つ大きい値
+    *
+    * @returns {int}} min 以上 max 未満 の整数値を「ランダムで」出力
+    */
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+// 1つめの引数を0, 2つ目の引数を3とすると, 0から2の整数がランダムで出力される
+console.log(getRandomInt(0, 3))
+
+// 上記のgetRandomInt関数と、配列 hands を生かしてジャンケンゲームを作成してみましょう！
+function game(player){
+	x = getRandomInt(0,3);
+	y = hands.indexOf(player);
+	console.log("わたしは"+player)
+	console.log(`相手は${hands[x]}`)
+	if(x===y){
+		console.log("あいこ")
+		return "あいこ";
+	}else if(y===0 && x===1 || y===1 && x===2 || y===2 && x===0){
+		console.log("かち")
+		return "かち";
+	}else{
+		console.log("まけ")
+		return "まけ"
+	}
+}
+test(game("グー"),"かち");
+test(game("グー"),"まけ");
+test(game("グー"),"あいこ");
+
+game("グー");
+game("チョキ");
+game("パー");
