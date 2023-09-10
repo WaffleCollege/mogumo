487d489ee20832a0c6fa23709b344393e696f0cf
kurumayusamugariko <junmomo6780@gmail.com>
2023-09-11 02:13:56 +0900

    追加

--- P05/object-intro/script.js
+++ P05/object-intro/script.js
@@ -1,3 +1,20 @@
 'use strict'
 // Please don't delete the 'use strict' line above
 
+function test(actual, expected) {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log("Yay! Test PASSED.");
+  } else {
+    console.error("Test FAILED. Keep trying!");
+    console.log("    actual: ", actual);
+    console.log("  expected: ", expected);
+    console.trace();
+  }
+}
+
+//問題１
+
+
+
+// この演習では TDD スタイルのテストは必要ありません。ここでは console.log を使用してください。
+console.log(myInfo["isCoder"]); // => "true"
