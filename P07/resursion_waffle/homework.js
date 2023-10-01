fd75afb6b4002c988eae9272391b441a4cbac954
kurumayusamugariko <junmomo6780@gmail.com>
2023-10-02 07:18:04 +0900

    "07-1"完

--- P07/resursion_waffle/homework.js
+++ P07/resursion_waffle/homework.js
@@ -1,3 +1,4 @@
+/*
 const title = document.querySelector(".title");
 title.style.fontSize = "20px";
 
@@ -9,17 +10,56 @@ for(let t of titleAll){
 //文章消える
 let i = titleAll.length;
 for(let t of titleAll){
-	if(i%3 === 0 || i%4 === 0){
+	if(i%3 === 0 || j%4 === 0){
 		t.style.display = "none";
 	}
 	i--;
 }
 
 //番号消える
-let i = 0;
+i = 0;
 for(let t of titleAll){
 	if(i%2 === 0){
 		t.style.display = "none";
 	}
 	i++;
 }
+*/
+
+console.log("07-1,1");//07-1
+let theLoneliestNumber = 1;
+
+let aFriend = theLoneliestNumber;
+
+console.log(theLoneliestNumber); // 1
+console.log(aFriend); // 1
+console.log(theLoneliestNumber === aFriend); // true
+
+console.log("07-1,2")
+const kermit = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "violet",
+];
+const missPiggy = kermit.slice();
+
+console.log(kermit); // ["red","orange","yellow","green","blue","indigo","violet",]
+console.log(missPiggy); // ["red","orange","yellow","green","blue","indigo","violet",]
+console.log(kermit === missPiggy); // false
+
+console.log("07-1,3");
+let mamas = "Monday";
+
+let papas = mamas;
+
+papas += ", so good to me.";
+
+console.log(mamas); // "Monday"
+console.log(papas); // "Monday,so good for me"
+console.log(mamas === papas); // false
+
+console.log("07-1,4");
