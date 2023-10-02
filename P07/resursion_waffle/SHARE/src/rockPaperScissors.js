7f56e1439bf95f4c1a4468d777f4bb48ffa4bbaf
kurumayusamugariko <junmomo6780@gmail.com>
2023-10-03 01:53:21 +0900

    改良？

--- P07/resursion_waffle/SHARE/src/rockPaperScissors.js
+++ P07/resursion_waffle/SHARE/src/rockPaperScissors.js
@@ -26,11 +26,10 @@ function rockPaperScissors(rounds){
 	let out = [];
 	const weapons = ["rock","scissors","paper"];
 	const play = (rounds, result = []) => {
-
+		
 		if(rounds < 1) {//roundsが1未満の時終了
 			out.push(result);
-			console.log(result);
-			return;
+			return ;
 		}
 
 		for(let i =0; i < weapons.length; i++){
@@ -39,7 +38,7 @@ function rockPaperScissors(rounds){
 		}
 	};
 	play(rounds);
-	return out;
+	return console.log(out);
 };
 rockPaperScissors(3);
 
