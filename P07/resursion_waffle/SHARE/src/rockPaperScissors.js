42a728c50fe878d9d42a862d6de1190bd36b07cd
kurumayusamugariko <junmomo6780@gmail.com>
2023-10-03 01:30:42 +0900

    問題１

--- P07/resursion_waffle/SHARE/src/rockPaperScissors.js
+++ P07/resursion_waffle/SHARE/src/rockPaperScissors.js
@@ -1,5 +1,47 @@
 /* exported rockPaperScissors */
 
-const rockPaperScissors = () => {
-  // Your code here
+console.log("1-1");
+
+/*const rockPaperScissors = (rounds = 3) => {
+	let out = [];//consoleに表示
+	const weapons = ["rock","scissors","paper"];
+	const play = (rounds, result = []) => {
+
+		if(rounds < 1) {//roundsが1未満の時終了
+			out.push(result);
+			console.log(result);
+			return;
+		}
+
+		for(let i =0; i < weapons.length; i++){
+			const weapon = weapons[i];
+			play(rounds - 1,result.concat(weapon));
+		}
+	};
+	play(rounds);
+	return out;
+};*/
+
+function rockPaperScissors(rounds){
+	let out = [];
+	const weapons = ["rock","scissors","paper"];
+	const play = (rounds, result = []) => {
+
+		if(rounds < 1) {//roundsが1未満の時終了
+			out.push(result);
+			console.log(result);
+			return;
+		}
+
+		for(let i =0; i < weapons.length; i++){
+			const weapon = weapons[i];
+			play(rounds - 1,result.concat(weapon));
+		}
+	};
+	play(rounds);
+	return out;
 };
+rockPaperScissors(3);
+
+console.log("1-2");
+rockPaperScissors(4);
