9efe5dab102a0aa62133e85d143ade980cb80ad3
kurumayusamugariko <junmomo6780@gmail.com>
2024-01-11 17:31:22 +0900

    P14

---
+++ pokemon/SHARE/src/pokemonager.js
@@ -0,0 +1,28 @@
+const allPokemon = window.allPokemon;
+
+window.pokemonager = {
+  findNames: () => {
+    // 1問目のコードをここに書きましょう
+
+  },
+
+  findByResistance: (attack) => {
+    // 2問目のコードをここに書きましょう
+
+  },
+
+  findNamesByResistance: (attack) => {
+    // 3問目のコードをここに書きましょう
+
+  },
+
+  avgMaxCP: () => {
+    // 4問目のコードをここに書きましょう
+
+  },
+
+  countedWeaknesses: () => {
+    // 5問目のコードをここに書きましょう
+    
+  },
+};
